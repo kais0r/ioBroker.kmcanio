9d2b740db3d4c94dd8807119043503c8a4fdac8f
M.Eng. Franz Kaiser <franz.kaiser@kaiser-mechatronik.de>
2018-08-23 01:51:14 +0200

    first CAN IO test running

--- main.js
+++ main.js
@@ -96,33 +96,112 @@ function main() {
 
     // The adapters config (in the instance object everything under the attribute "native") is accessible via
     // adapter.config:
-    adapter.log.info('config test1: '    + adapter.config.test1);
-    adapter.log.info('config test1: '    + adapter.config.test2);
-    adapter.log.info('config mySelect: ' + adapter.config.mySelect);
+    adapter.log.info('config canioType: '    + adapter.config.canioType);
+    adapter.log.info('config canioNodeID: '  + adapter.config.canioNodeID);
+    
+	
+	
+	
+	
+    adapter.setObject('IN1.0', { type: 'state', common: { name: 'IN1.0', type: 'boolean', role: 'indicator' }, native: {} });
+    adapter.setObject('IN1.1', { type: 'state', common: { name: 'IN1.1', type: 'boolean', role: 'indicator' }, native: {} });
+    adapter.setObject('IN1.2', { type: 'state', common: { name: 'IN1.2', type: 'boolean', role: 'indicator' }, native: {} });
+    adapter.setObject('IN1.3', { type: 'state', common: { name: 'IN1.3', type: 'boolean', role: 'indicator' }, native: {} });
+    adapter.setObject('IN1.4', { type: 'state', common: { name: 'IN1.4', type: 'boolean', role: 'indicator' }, native: {} });
+    adapter.setObject('IN1.5', { type: 'state', common: { name: 'IN1.5', type: 'boolean', role: 'indicator' }, native: {} });
+    adapter.setObject('IN1.6', { type: 'state', common: { name: 'IN1.6', type: 'boolean', role: 'indicator' }, native: {} });
+    adapter.setObject('IN1.7', { type: 'state', common: { name: 'IN1.7', type: 'boolean', role: 'indicator' }, native: {} });
+	
+    adapter.setObject('IN2.0', { type: 'state', common: { name: 'IN2.0', type: 'boolean', role: 'indicator' }, native: {} });
+    adapter.setObject('IN2.1', { type: 'state', common: { name: 'IN2.1', type: 'boolean', role: 'indicator' }, native: {} });
+    adapter.setObject('IN2.2', { type: 'state', common: { name: 'IN2.2', type: 'boolean', role: 'indicator' }, native: {} });
+    adapter.setObject('IN2.3', { type: 'state', common: { name: 'IN2.3', type: 'boolean', role: 'indicator' }, native: {} });
+    adapter.setObject('IN2.4', { type: 'state', common: { name: 'IN2.4', type: 'boolean', role: 'indicator' }, native: {} });
+    adapter.setObject('IN2.5', { type: 'state', common: { name: 'IN2.5', type: 'boolean', role: 'indicator' }, native: {} });
+    adapter.setObject('IN2.6', { type: 'state', common: { name: 'IN2.6', type: 'boolean', role: 'indicator' }, native: {} });
+    adapter.setObject('IN2.7', { type: 'state', common: { name: 'IN2.7', type: 'boolean', role: 'indicator' }, native: {} });
+	
+    adapter.setObject('IN3.0', { type: 'state', common: { name: 'IN3.0', type: 'boolean', role: 'indicator' }, native: {} });
+    adapter.setObject('IN3.1', { type: 'state', common: { name: 'IN3.1', type: 'boolean', role: 'indicator' }, native: {} });
+    adapter.setObject('IN3.2', { type: 'state', common: { name: 'IN3.2', type: 'boolean', role: 'indicator' }, native: {} });
+    adapter.setObject('IN3.3', { type: 'state', common: { name: 'IN3.3', type: 'boolean', role: 'indicator' }, native: {} });
+    adapter.setObject('IN3.4', { type: 'state', common: { name: 'IN3.4', type: 'boolean', role: 'indicator' }, native: {} });
+    adapter.setObject('IN3.5', { type: 'state', common: { name: 'IN3.5', type: 'boolean', role: 'indicator' }, native: {} });
+    adapter.setObject('IN3.6', { type: 'state', common: { name: 'IN3.6', type: 'boolean', role: 'indicator' }, native: {} });
+    adapter.setObject('IN3.7', { type: 'state', common: { name: 'IN3.7', type: 'boolean', role: 'indicator' }, native: {} });
+	
+    adapter.setObject('IN4.0', { type: 'state', common: { name: 'IN4.0', type: 'boolean', role: 'indicator' }, native: {} });
+    adapter.setObject('IN4.1', { type: 'state', common: { name: 'IN4.1', type: 'boolean', role: 'indicator' }, native: {} });
+    adapter.setObject('IN4.2', { type: 'state', common: { name: 'IN4.2', type: 'boolean', role: 'indicator' }, native: {} });
+    adapter.setObject('IN4.3', { type: 'state', common: { name: 'IN4.3', type: 'boolean', role: 'indicator' }, native: {} });
+	
+    adapter.setObject('Temperatur', { type: 'state', common: { name: 'Temperatur1', type: 'number', role: 'indicator' }, native: {} });
+	
+	
+	
+	
+	
+	
+	var can = require('socketcan');
+	var bus = can.createRawChannel("can0", true);
+	
+	
+	bus.addListener("onMessage", 
+		function(msg) { 
+			if( msg.id == 0x191 ){
+				
+				if(  msg.data[0] & 0x01 ){ adapter.setState('IN1.0', {val: true, ack: true}); }else{ adapter.setState('IN1.0', {val: false, ack: true}); }
+				if(  msg.data[0] & 0x02 ){ adapter.setState('IN1.1', {val: true, ack: true}); }else{ adapter.setState('IN1.1', {val: false, ack: true}); }
+				if(  msg.data[0] & 0x04 ){ adapter.setState('IN1.2', {val: true, ack: true}); }else{ adapter.setState('IN1.2', {val: false, ack: true}); }
+				if(  msg.data[0] & 0x08 ){ adapter.setState('IN1.3', {val: true, ack: true}); }else{ adapter.setState('IN1.3', {val: false, ack: true}); }
+				if(  msg.data[0] & 0x10 ){ adapter.setState('IN1.4', {val: true, ack: true}); }else{ adapter.setState('IN1.4', {val: false, ack: true}); }
+				if(  msg.data[0] & 0x20 ){ adapter.setState('IN1.5', {val: true, ack: true}); }else{ adapter.setState('IN1.5', {val: false, ack: true}); }
+				if(  msg.data[0] & 0x40 ){ adapter.setState('IN1.6', {val: true, ack: true}); }else{ adapter.setState('IN1.6', {val: false, ack: true}); }
+				if(  msg.data[0] & 0x80 ){ adapter.setState('IN1.7', {val: true, ack: true}); }else{ adapter.setState('IN1.7', {val: false, ack: true}); }
+				
+				if(  msg.data[1] & 0x01 ){ adapter.setState('IN2.0', {val: true, ack: true}); }else{ adapter.setState('IN2.0', {val: false, ack: true}); }
+				if(  msg.data[1] & 0x02 ){ adapter.setState('IN2.1', {val: true, ack: true}); }else{ adapter.setState('IN2.1', {val: false, ack: true}); }
+				if(  msg.data[1] & 0x04 ){ adapter.setState('IN2.2', {val: true, ack: true}); }else{ adapter.setState('IN2.2', {val: false, ack: true}); }
+				if(  msg.data[1] & 0x08 ){ adapter.setState('IN2.3', {val: true, ack: true}); }else{ adapter.setState('IN2.3', {val: false, ack: true}); }
+				if(  msg.data[1] & 0x10 ){ adapter.setState('IN2.4', {val: true, ack: true}); }else{ adapter.setState('IN2.4', {val: false, ack: true}); }
+				if(  msg.data[1] & 0x20 ){ adapter.setState('IN2.5', {val: true, ack: true}); }else{ adapter.setState('IN2.5', {val: false, ack: true}); }
+				if(  msg.data[1] & 0x40 ){ adapter.setState('IN2.6', {val: true, ack: true}); }else{ adapter.setState('IN2.6', {val: false, ack: true}); }
+				if(  msg.data[1] & 0x80 ){ adapter.setState('IN2.7', {val: true, ack: true}); }else{ adapter.setState('IN2.7', {val: false, ack: true}); }
+				
+				if(  msg.data[2] & 0x01 ){ adapter.setState('IN3.0', {val: true, ack: true}); }else{ adapter.setState('IN3.0', {val: false, ack: true}); }
+				if(  msg.data[2] & 0x02 ){ adapter.setState('IN3.1', {val: true, ack: true}); }else{ adapter.setState('IN3.1', {val: false, ack: true}); }
+				if(  msg.data[2] & 0x04 ){ adapter.setState('IN3.2', {val: true, ack: true}); }else{ adapter.setState('IN3.2', {val: false, ack: true}); }
+				if(  msg.data[2] & 0x08 ){ adapter.setState('IN3.3', {val: true, ack: true}); }else{ adapter.setState('IN3.3', {val: false, ack: true}); }
+				if(  msg.data[2] & 0x10 ){ adapter.setState('IN3.4', {val: true, ack: true}); }else{ adapter.setState('IN3.4', {val: false, ack: true}); }
+				if(  msg.data[2] & 0x20 ){ adapter.setState('IN3.5', {val: true, ack: true}); }else{ adapter.setState('IN3.5', {val: false, ack: true}); }
+				if(  msg.data[2] & 0x40 ){ adapter.setState('IN3.6', {val: true, ack: true}); }else{ adapter.setState('IN3.6', {val: false, ack: true}); }
+				if(  msg.data[2] & 0x80 ){ adapter.setState('IN3.7', {val: true, ack: true}); }else{ adapter.setState('IN3.7', {val: false, ack: true}); }
+					
+				if(  msg.data[3] & 0x01 ){ adapter.setState('IN4.0', {val: true, ack: true}); }else{ adapter.setState('IN4.0', {val: false, ack: true}); }
+				if(  msg.data[3] & 0x02 ){ adapter.setState('IN4.1', {val: true, ack: true}); }else{ adapter.setState('IN4.1', {val: false, ack: true}); }
+				if(  msg.data[3] & 0x04 ){ adapter.setState('IN4.2', {val: true, ack: true}); }else{ adapter.setState('IN4.2', {val: false, ack: true}); }
+				if(  msg.data[3] & 0x08 ){ adapter.setState('IN4.3', {val: true, ack: true}); }else{ adapter.setState('IN4.3', {val: false, ack: true}); }
+				
+				var temp = (msg.data[4] + msg.data[5] * 256) * 0.0625;
+				adapter.setState('Temperatur', {val: temp, ack: true});
+				
+			}
+		} 
+	);
+	
+	
+	bus.start();
+	
+	
+	var buf = new Buffer([1,2,3,4,5,6,7,8]);
+	
+	bus.send({ id: 12, ext: false, data: buf });
+	
+	
 
-
-    /**
-     *
-     *      For every state in the system there has to be also an object of type state
-     *
-     *      Here a simple kmcanio for a boolean variable named "testVariable"
-     *
-     *      Because every adapter instance uses its own unique namespace variable names can't collide with other adapters variables
-     *
-     */
-
-    adapter.setObject('testVariable', {
-        type: 'state',
-        common: {
-            name: 'testVariable',
-            type: 'boolean',
-            role: 'indicator'
-        },
-        native: {}
-    });
 
     // in this kmcanio all states changes inside the adapters namespace are subscribed
-    adapter.subscribeStates('*');
+//    adapter.subscribeStates('*');
 
 
     /**
@@ -133,25 +212,15 @@ function main() {
      */
 
     // the variable testVariable is set to true as command (ack=false)
-    adapter.setState('testVariable', true);
+ //   adapter.setState('testVariable', true);
 
     // same thing, but the value is flagged "ack"
     // ack should be always set to true if the value is received from or acknowledged from the target system
-    adapter.setState('testVariable', {val: true, ack: true});
+    
 
     // same thing, but the state is deleted after 30s (getState will return null afterwards)
-    adapter.setState('testVariable', {val: true, ack: true, expire: 30});
+  //  adapter.setState('testVariable', {val: true, ack: true, expire: 30});
 
-
-
-    // examples for the checkPassword/checkGroup functions
-    adapter.checkPassword('admin', 'iobroker', function (res) {
-        console.log('check user admin pw ioboker: ' + res);
-    });
-
-    adapter.checkGroup('admin', 'admin', function (res) {
-        console.log('check group user admin group admin: ' + res);
-    });
 
 
 
